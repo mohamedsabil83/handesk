ebed443bce4a2a90408bce3f8df1e530667a53aa
MohamedSabil83 <me@mohamedsabil83.com>
2022-02-23 01:54:45 +0300

    Sync root files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -6,22 +6,26 @@ let mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.babel([
-        //'resources/assets/js/app.js',
+// mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+//
+// ]);
+
+mix.babel(
+    [
+        //'resources/js/app.js',
         'vendor/badchoice/thrust/src/resources/js/thrust.min.js',
-        'resources/assets/js/utils.js',
-        'resources/assets/js/libs/jquery.tagsinput.min.js',  //http://xoxco.com/projects/code/tagsinput/
-        'resources/assets/js/libs/mention.js/bootstrap-typeahead.js',  //https://github.com/ivirabyan/jquery-mentions
-        'resources/assets/js/libs/mention.js/mention.js',  //https://github.com/ivirabyan/jquery-mentions
-    ], 'public/js/app.js')
-    .babel('resources/assets/js/moment.min.js'                 ,'public/js/moment.js')
-    .less('resources/assets/less/style.less',                  '../resources/assets/css/style.css')
-    .styles([
-        'resources/assets/css/libs/jquery.tagsinput.min.css',
-        'resources/assets/css/style.css'
-    ],'public/css/all.css');
+        'resources/js/utils.js',
+        'resources/js/libs/jquery.tagsinput.min.js', //http://xoxco.com/projects/code/tagsinput/
+        'resources/js/libs/mention.js/bootstrap-typeahead.js', //https://github.com/ivirabyan/jquery-mentions
+        'resources/js/libs/mention.js/mention.js', //https://github.com/ivirabyan/jquery-mentions
+    ],
+    'public/js/app.js',
+)
+    .babel('resources/js/moment.min.js', 'public/js/moment.js')
+    .less('resources/less/style.less', '../resources/css/style.css')
+    .styles(['resources/css/libs/jquery.tagsinput.min.css', 'resources/css/style.css'], 'public/css/all.css');
