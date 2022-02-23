c150b648f4bda8e639fb4b062293d58330d8bcc0
MohamedSabil83 <me@mohamedsabil83.com>
2022-02-23 12:51:11 +0300

    Add cdn link for jQuery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,10 @@
     </div>
 
     <!-- Scripts -->
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        referrerpolicy="no-referrer"
+    ></script>
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('scripts')
     @stack('edit-scripts')
